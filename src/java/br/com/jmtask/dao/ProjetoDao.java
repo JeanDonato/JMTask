18b84a380dbbf48985ea577e108d5b1f4260fb90
JeanDonato <jeancarlosgoncalves@gmail.com>
2011-06-03 19:23:59 -0300

    *desacoplamento de codigo *nova namedQuery criada em tarefa para pesquisa de tarefa por colaborador, projeto *parametros passados por MAP agora

---
+++ src/java/br/com/jmtask/dao/ProjetoDao.java
@@ -0,0 +1,94 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.jmtask.dao;
+
+import br.com.jmtask.entity.Projeto;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.UserTransaction;
+
+/**
+ *
+ * @author jeandonato
+ */
+public class ProjetoDao {
+
+    @PersistenceContext
+    private EntityManager em;
+    @Resource
+    UserTransaction ut;
+
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    public void salvar(Projeto projeto) {
+        try {
+            ut.begin();
+            em.persist(projeto);
+            ut.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("######################### Nao conseguiu salvar projeto #########################");
+        }
+    }
+
+    public void atualizar(Projeto projeto) {
+        try {
+            ut.begin();
+            em.merge(projeto);
+            ut.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("######################### Nao conseguiu atualizar projeto #########################");
+        }
+    }
+
+    public List<Projeto> getProjetos() {
+        Query query = em.createNamedQuery("Projeto.findAll");
+        return query.getResultList();
+    }
+
+    public void remover(Projeto projeto) {
+        try {
+            ut.begin();
+            em.remove(em.getReference(Projeto.class, projeto.getId()));
+            ut.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("######################### Nao conseguiu remover projeto #########################");
+        }
+    }
+
+    public boolean projetoTemTarefa(Projeto projeto) {
+        Query query = em.createNamedQuery("Tarefa.findByProjeto");
+        query.setParameter("projeto", projeto);
+        if (query.getResultList().size() > 0) {
+            System.out.println("################# Projeto tem tarefa, nao podera ser removido #################");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Projeto findByNome(String nomeProjeto) {
+        Query query = em.createNamedQuery("Projeto.verifProjeto");
+        query.setParameter("nomeProjeto", nomeProjeto);
+        try {
+            return (Projeto) query.getSingleResult();
+        } catch (NoResultException nre) {
+            System.out.println("pesquisa sem nenhum resultado para esse nomeProjeto");
+            return null;
+        }
+    }
+}
---
+++ src/java/br/com/jmtask/dao/TarefaDao.java
@@ -0,0 +1,100 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.jmtask.dao;
+
+import br.com.jmtask.entity.LogTarefa;
+import br.com.jmtask.entity.Tarefa;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.UserTransaction;
+
+/**
+ *
+ * @author jeandonato
+ */
+public class TarefaDao {
+
+    @PersistenceContext
+    private EntityManager em;
+    @Resource
+    UserTransaction ut;
+
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    public void salvar(Tarefa tarefa) {
+        try {
+            ut.begin();
+            em.persist(tarefa);
+            ut.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("######################### Nao conseguiu salvar tarefa #########################");
+        }
+    }
+
+    public void atualizar(Tarefa tarefa) {
+        try {
+            ut.begin();
+            em.merge(tarefa);
+            ut.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("######################### Nao conseguiu atualizar tarefa #########################");
+        }
+    }
+
+    public List<Tarefa> getTarefas(String sql, Map<String, Object> parametros) {
+
+        Query query = em.createNativeQuery(sql, Tarefa.class);
+
+        if (!parametros.isEmpty()) {
+            if (parametros.containsKey("colaborador")) {
+                query.setParameter("colaborador", parametros.get("colaborador"));
+            }
+            if (parametros.containsKey("status")) {
+                query.setParameter("status", parametros.get("status"));
+            }
+            if (parametros.containsKey("projeto")) {
+                query.setParameter("projeto", parametros.get("projeto"));
+            }
+        }
+        return query.getResultList();
+    }
+
+    public Tarefa getTarefa(Long id) {
+        Query query = em.createNamedQuery("Tarefa.findById");
+        query.setParameter("id", id);
+        return (Tarefa) query.getSingleResult();
+    }
+
+// ################# LOG DAS TAREFAS #################
+    public List<LogTarefa> getLogsDaTarefaSelecionada(Tarefa tarefa) {
+        Query query = em.createNamedQuery("LogTarefa.findByTarefa");
+        query.setParameter("tarefa", tarefa);
+        return query.getResultList();
+    }
+
+    public void SalvarLog(LogTarefa logTarefa) {
+        try {
+            ut.begin();
+            em.persist(logTarefa);
+            ut.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    // ################# LOG DAS TAREFAS #################
+}
